13bf6a4e8dd913c82a598fd9b4e624758089c274
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-02-07 15:42:48 +0000

    Adding task_item class, and create superuser

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class task_item(models.Model):
+    task = models.CharField(max_length=300, null=False, blank=False)
+    owner = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+    
+
