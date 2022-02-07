2c38ee266644573666b24372b833137bfa95051f
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-02-07 17:15:39 +0000

    Adding table to display tasks

--- manager/models.py
+++ manager/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 
-class task_item(models.Model):
+
+class Item(models.Model):
     task = models.CharField(max_length=300, null=False, blank=False)
     owner = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
-    
+
+    def __str__(self):
+        return self.task
 
--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
-def get_manager_list(request):
-    return render(request, 'manager/manager_list.html')
 
+
+def get_manager_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'manager/manager_list.html', context)
