2aefa9ddb84afac06a5d1c62e44e2ddc1237dc4e
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-02-14 09:45:51 +0000

    Fixing test bug with redirect

--- manager/test_views.py
+++ manager/test_views.py
@@ -5,34 +5,35 @@ from .models import Item
 # Create your tests here.
 
 class TestViews(TestCase):
-    """[return a successful HTTP response]"""
+    """[Testing views]"""
 
     def test_get_manager_lists(self):
-        """[return a successful HTTP response]"""
+        """[return a successful manager_list HTTP response]"""
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'manager/manager_list.html')
         
     def test_get_add_item(self):
-        """[return a successful HTTP response]"""
-        response = self.client.get('/add')
+        """[return a successful add_item HTTP response]"""
+        response = self.client.get('/add', {'task': 'Test', 'owner':'Tester', 'due_date':'2021-05-19T01:55:10+0000', 'done':'True'}, True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'manager/add_item.html')
 
     def test_get_edit_item(self):
-        """[return a successful HTTP response]"""
+        """[return a successful edit_item HTTP response]"""
         item = Item.objects.create(task='Test', owner="Tester", due_date="2021-05-19T01:55:10+0000", done=True)
         response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'manager/edit_item.html')
         
     def test_can_add_item(self):
-        
-        response = self.client.post('/add', {'task': 'Test', 'owner':'Tester', 'due_date':'2021-05-19T01:55:10+0000', 'done':'True'})
+        """[test if test can add item]"""
+        response = self.client.post('/add', {'task': 'Test', 'owner':'Tester', 'due_date':'2021-05-19T01:55:10+0000', 'done':'True'}, follow=True)
         self.assertRedirects(response, '/')
 
         
     def test_can_delete_item(self):
+        """[test if test can delete item]"""
         item = Item.objects.create(task='Test', owner="Tester", due_date="2021-05-19T01:55:10+0000", done=True)
         response = self.client.get(f'/delete/{item.id}')
         self.assertRedirects(response, '/')
@@ -40,6 +41,7 @@ class TestViews(TestCase):
         self.assertEqual(len(existing_items), 0)
         
     def test_can_toggle_item(self):
+        """[test if test can toggle item]"""
         item = Item.objects.create(task='Test', owner="Tester", due_date="2021-05-19T01:55:10+0000", done=True)
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
