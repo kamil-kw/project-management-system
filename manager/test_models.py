8d83d9b37cd43102d9126b0fce82ccd2fec3a6fe
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-02-14 11:17:51 +0000

    Adding TestModels class

--- manager/test_models.py
+++ manager/test_models.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Item
 
 
+class TestModels(TestCase):
+    """[Testing models]"""
+    def test_done_defaults_to_false(self):
+        """[Testing if manager items will be created by default]"""
+        item = Item.objects.create(task='Test', owner="Tester", due_date="2021-05-19T01:55:10+0000")
+        self.assertFalse(item.done)
