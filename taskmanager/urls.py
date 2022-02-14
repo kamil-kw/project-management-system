ca226817cf31cb59fb8dba573af2ef5d8196b4c8
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-02-14 19:29:23 +0000

    Adding links to navbar

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('add/', views.add_item, name='add'),
     path('edit/<item_id>', views.edit_item, name='edit'),
     path('toggle/<item_id>', views.toggle_item, name='toggle'),
-    path('delete/<item_id>', views.delete_item, name='toggle'),
+    path('delete/<item_id>', views.delete_item, name='delete'),
     path('accounts/', include('allauth.urls'))
 ]
