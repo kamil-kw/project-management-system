11e1eabad8cc413f23ede64541a2504ff7797eb4
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-02-21 16:42:14 +0000

    Changing static settings

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -146,12 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILE_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-DEFAULT_FILELD_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Default primary key field type
