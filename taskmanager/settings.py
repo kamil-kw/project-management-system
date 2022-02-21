61c880b12477878d4dcd4f7c83524eab7174216b
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-02-21 20:46:46 +0000

    Changing settings

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from django.urls import reverse_lazy
 if os.path.isfile('env.py'):
     import env
 
